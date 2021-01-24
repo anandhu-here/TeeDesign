31ccf2ff5d81fda52c778519ec086fad3fb1374c
anandhu <anandhuuhere@gmail.com>
2021-01-24 16:31:20 +0530

    github deploy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Home from './Layout/Home';
+import Navbar from './Layout/Navbar';
 import './styles/main.css';
 function App() {
   return (
     <div className="App">
+    <Navbar/>
       <Home />
     </div>
   );
